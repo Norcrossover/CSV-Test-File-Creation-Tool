8a5714e7219406fc708607d4dba9c73a76672848
Kobe <kobenorcross@gmail.com>
2021-09-01 22:04:11 -0700

    addd test variables and a function to create a test file

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,23 @@
-import { columns, delimiter, lines } from "./getInput"
-console.log(columns, delimiter, lines);
+// import { columns, delimiter, lines } from "./getInput"
+// console.log(columns, delimiter, lines);
+
+import fs from 'fs';
+let columns = 5;
+let delimiter = ';';
+let lines = 1000;
+const alphabet = [
+    a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
+]
+const data =  '';
+const create => (columns, delimiter, lines) {
+    let str = '';
+    for (let i = 0; i < lines; i++) {
+        for (let j = 0; j < columns; j++) {
+            
+        }
+        
+    }
+}
+fs.writeFile('CSV_Test_File.txt', data, (err) => {
+    if (err) throw err;
+});
