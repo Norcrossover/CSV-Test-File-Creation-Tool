944bbe0b83be38cfcdd94ff9f2a01b4861efe537
Kobe <kobenorcross@gmail.com>
2021-09-01 19:19:49 -0700

    Added this file to import user input from the webpage

---
+++ scripts/getInput.js
@@ -0,0 +1,6 @@
+// Have options to bring in input from buttons here:
+let columns = document.getElementById("Columns");
+let delimiter = document.getElementById("Delimiter");
+let lines = document.getElementById("Lines of Text");
+
+export {columns, delimiter, lines};
