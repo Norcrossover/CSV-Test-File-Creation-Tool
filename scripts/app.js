3477ecb56f4d73a9a73dd504c27b217d2ffad07d
Kobe <kobenorcross@gmail.com>
2021-09-01 22:22:19 -0700

    Fixed possiblr calls to wrong functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,38 @@
 // import { columns, delimiter, lines } from "./getInput"
 // console.log(columns, delimiter, lines);
 
-import fs from 'fs';
+//import fs from 'fs';
+const fs = require('fs')
 let columns = 5;
 let delimiter = ';';
 let lines = 1000;
 const alphabet = [
-    a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
+    'a', 'b', 'c', 'd', 'e', 
+    'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 
+    'p', 'q', 'r', 's', 't', 
+    'u', 'v', 'w', 'x', 'y', 
+    'z'
 ];
-const data =  '';
+let data =  '';
 const create = (columns, delimiter, lines) => {
+    let text = '';
     for (let i = 0; i < lines; i++) {
+        let str = '';
         for (let j = 0; j < columns; j++) {
-            let str = '';
-            let strLen = RandomSource() % 25;
+            str = '';
+            let strLen = Math.random() % 25;
             for (let k = 0; k < strLen; k++) {
-                str.concat(alphabet[RandomSource() % 25]);
+                str.concat(alphabet[Math.random() % 25]);
             }
             str.concat(str + delimiter);
         }
-        data.concat(str + '\n');
+        text.concat(str + '\n');
     }
+    return text;
 }
+data = create(columns, delimiter, lines);
+console.log(data);
 fs.writeFile('CSV_Test_File.txt', data, (err) => {
     if (err) throw err;
 });
