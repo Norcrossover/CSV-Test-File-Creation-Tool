6f41686f6338849a6091e6ec88bf3ea86b21ef14
Kobe <kobenorcross@gmail.com>
2021-09-14 11:17:50 -0600

    added console logs

--- scripts/getInput.js
+++ scripts/getInput.js
@@ -4,4 +4,5 @@ let delimiter = document.getElementById("Delimiter");
 let lines = document.getElementById("Lines of Text");
 console.log(`columns: ${columns}`);
 console.log(`delimiter: ${delimiter}`);
+console.log(`lines: ${lines}`);
 export { columns, delimiter, lines };
